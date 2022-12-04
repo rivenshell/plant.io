fc1a387269381d01b181ab2088281cc900587735
Riv <53383837+rivenshell@users.noreply.github.com>
2022-12-04 02:05:47 -0500

    cover

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
    <>
       <div className='app'>
         <Navbar />
-        
+        <img src='https://images.unsplash.com/photo-1611843467160-25afb8df1074?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
         </div>
     {/* <nav class="navbar bg-light">
     <div class="container-fluid">
