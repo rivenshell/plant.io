f1a095e7e334bd6154464fa1b42b7eec54820bc0
Riv <53383837+rivenshell@users.noreply.github.com>
2022-12-04 01:57:32 -0500

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './styles/App.css'
 import { Routes, Route } from 'react-router-dom'
-import bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Navbar from './components/navbar'
 
 function App() {
   <Routes>
@@ -13,13 +15,29 @@ function App() {
 
   return (
    <>
-   <div className='app'>
-      <h2>Is anyone here?</h2>
+      <div className='app'>
+        <Navbar />
+        
+        </div>
+    {/* <nav class="navbar bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand">Plant.io</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    </div>
+    </nav> */}
+   
+     <div className='wrapper'>
 
     </div>
 
+
     
-   </> 
+   </>
+
+
   )
 }
 
